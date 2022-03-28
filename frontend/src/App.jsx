59cd5aee20086c4ec756e8554300ea2dbae82f0e
FinnGlobal <finn.he0102@gmail.com>
2022-03-28 23:03:05 +1300

    App.module.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import apps from './App.module.css'
 
 export default class App extends Component {
   render() {
     return (
-      <div>try try </div>
+      <div>
+        <div className={apps.navigateArea}></div>
+        <div className={apps.displayArea}></div>
+      </div>
     )
   }
 }
