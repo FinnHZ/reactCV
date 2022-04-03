b53bf9d941deb11f33cd3096db5b8d3912e90461
FinnGlobal <finn.he0102@gmail.com>
2022-04-03 17:45:52 +1200

    axios json

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/coverletter', methods=['GET','POST'])
+def coverletter():
+    coverletterInfo = {}
+    coverletterInfo['content'] = 'letter content!!!!!!1'
+    coverletterInfo_json = json.dumps(coverletterInfo)
+    return coverletterInfo_json
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port=5000, debug=True)
