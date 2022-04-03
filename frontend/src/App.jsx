2c1b8fec2bed7d677e97071e07359ab50be1c1e4
FinnGlobal <finn.he0102@gmail.com>
2022-04-03 17:44:25 +1200

    axios, navtitle, coverletter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,20 +15,37 @@ import developerImage from './img/developerImage.jpg'
 export default class App extends Component {
   // state={boundarytest:''}
   // static getDerivedStateFromError(error){ return {boundarytest: error}}
+
+  state = {}
+
+  toOtherLink = (otherlink) => {
+    return ( ()=>{
+      window.open(otherlink)
+    }) 
+  }
+
   render() {
     // const testboundary = <h2>兜底善后文字</h2>
     return (
       <Fragment>
         <div className={apps.navigateArea}> 
-          <div className={apps.navtitle} style={{width:"100%"}}>
-            <img src={developerImage} alt="Developer" style={{width:"30%", height:"30%"}}/>
-
+          <div className={apps.navtitle}>
+              <img className={apps.developer} src={developerImage} alt="Developer"/>
+              <div className={apps.develpoerFont}>
+                Full-Stack Developer 
+               & Software Developer
+              </div>
+              <div className={apps.contact}>
+                <div>LinkedIn: <button className={apps.linkButton} onClick={this.toOtherLink("https://www.linkedin.com/in/chiyu-he-3b1695232/")}>Click me</button></div>
+                <div>GitHub:<button className={apps.linkButton}  onClick={this.toOtherLink("https://github.com/FinnHZ")}>Click me</button></div>
+                <div>E-mail: finn.he0102@gmail.com</div>
+              </div>
           </div>
           <hr/>
           <div className="nav flex-column nav-pills me-3"  aria-orientation="vertical" style={{width:"100%"}}>
               <MyNavLink to="/HomePage">Home</MyNavLink>
-              <MyNavLink to="/BasicInfo">Basic Information</MyNavLink>
               <MyNavLink to="/CoverLetter">Cover Letter</MyNavLink>
+              <MyNavLink to="/BasicInfo">Basic Information</MyNavLink>
               <MyNavLink to="/Education">Education</MyNavLink>
               <MyNavLink to="/Experience">Experience</MyNavLink>
               <MyNavLink to="/Skills">Skills & Demo</MyNavLink>
@@ -41,8 +58,8 @@ export default class App extends Component {
         <div className={apps.displayArea}>
           <Routes>
               <Route path="/HomePage" element={<HomePage/>}/>
-              <Route path="/BasicInfo" element={<BasicPage/>}/>
               <Route path="/CoverLetter" element={<CoverLetterPage/>}/>
+              <Route path="/BasicInfo" element={<BasicPage/>}/>
               <Route path="/Education" element={<EducationPage/>}/>
               <Route path="/Experience" element={<ExperiencePage/>}/>
               <Route path="/Skills" element={<SkillsPage/>}/>
--- frontend/src/pages/Cover_Pg/index.jsx
+++ frontend/src/pages/Cover_Pg/index.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import covers from './index.module.css'
+import axios from 'axios'
 
 export default class CoverLetterPage extends Component {
+
+  componentDidMount() {
+      axios.get("http://127.0.0.1:5000/coverletter").then(
+          response => {console.log('successfully', response.data)},
+          error => {console.log('Unsuccessfully')}
+      )
+  }
+
+
+
   render() {
     return (
-      <div>CoverLetterPage</div>
+      <div style={{height:"100%"}}>
+        <div className={covers.letterBackground}></div>
+      </div>
     )
   }
 }
