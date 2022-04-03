a2ba87dfdd30e208466aad71f34a8c53e2303531
FinnGlobal <finn.he0102@gmail.com>
2022-04-03 22:31:21 +1200

    coverletter

--- frontend/src/pages/Cover_Pg/index.jsx
+++ frontend/src/pages/Cover_Pg/index.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
 import covers from './index.module.css'
-import axios from 'axios'
+// import axios from 'axios'
+import moment from 'moment'
 
 export default class CoverLetterPage extends Component {
 
+  // componentDidMount() {
+  //     axios.get("http://127.0.0.1:5000/coverletter").then(
+  //         response => {console.log('successfully', response.data)},
+  //         error => {console.log('Unsuccessfully')}
+  //     )
+  // }
+  
+  state = {currentDate:''}
+
   componentDidMount() {
-      axios.get("http://127.0.0.1:5000/coverletter").then(
-          response => {console.log('successfully', response.data)},
-          error => {console.log('Unsuccessfully')}
-      )
+    const dateDict = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    const currentDate = moment().date().toString() + " " + dateDict[moment().month()] + " " + moment().year().toString()
+    this.setState({currentDate})
   }
 
 
 
   render() {
+    const {currentDate} = this.state
     return (
       <div style={{height:"100%"}}>
-        <div className={covers.letterBackground}></div>
+        <div className={covers.letterBackground}>
+          <div className={covers.currentDate}>{currentDate}</div>
+          <div className={covers.letterAsk}>
+            Dear <div className={covers.companyName}></div>
+          </div>
+          <div className={covers.letterContent}></div>
+          <div className={covers.letterLast}>
+            Best Regards,
+              Finn
+          </div>
+        </div>
       </div>
     )
   }
