7be2e586e568233a0aa273fe6a5452ba9d078f9a
FinnGlobal <finn.he0102@gmail.com>
2022-05-04 22:13:08 +1200

    make sure the data structure

--- frontend/src/pages/Cover_Pg/index.jsx
+++ frontend/src/pages/Cover_Pg/index.jsx
@@ -8,7 +8,7 @@ class CoverLetterPage extends Component {
 
 
   
-  state = {currentDate:"", companyName:"developer"}
+  state = {currentDate:"", companyName:"developer", letterContent:[]}
 
   componentDidMount() {
     // axios.get("http://127.0.0.1:5000/coverletter").then(
@@ -20,17 +20,25 @@ class CoverLetterPage extends Component {
     const currentDate = moment().date().toString() + " " + dateDict[moment().month()] + " " + moment().year().toString()
     this.setState({currentDate})
 
+    const letterContentData = {
+      content_0: "This is a special cover letter for you. I'm Finn who is from China and a new man for IT. I want to be a full-stack developer, of course, I'm also can do software development if the software is interesting.",
+      content_1: "Actually, I was a Hotel Product Manager of the biggest online travel agency (Trip.com Group) in China 2.5 years ago. However, I want to see a more interesting world, so I come to  New Zealand through a WHV chance. I met some developers in my workplace in New Zealand. I found programming in NZ is a very interesting thing after communicating with them, so I start my IT life with the Master of Applied Computing at Lincoln University.",
+      content_2: "For the past 1.5 years, I have gotten good programming skills and good intern developer experience. The reason why I create this website and looking for a programming job is not only because I just got a Master's degree in Applied Computing from Lincoln University, but also because I like the feeling of creating websites and software. The joy from creating can make me keep moving.",
+      content_3: "I want to create more websites and software that can bring more joy and convenience to people. If that is also what you want, please see more detail about me. If I am the developer you want, please contact me through my e-mail. I would enjoy having the opportunity to discuss more with you and how I could use my skills to benefit our team. I look forward to hearing from you. "
+      }
+    this.setState({letterContent:letterContentData})
+
   }
 
 
 
   render() {
     // , companyName
-    const {currentDate} = this.state    
-    const content_0 = "This is a special cover letter for you. I'm Finn who is from China and a new man for IT. I want to be a full-stack developer, of course, I'm also can do software development if the software is interesting."
-    const content_1 = "Actually, I was a Hotel Product Manager of the biggest online travel agency (Trip.com Group) in China 2.5 years ago. However, I want to see a more interesting world, so I come to  New Zealand through a WHV chance. I met some developers in my workplace in New Zealand. I found programming in NZ is a very interesting thing after communicating with them, so I start my IT life with the Master of Applied Computing at Lincoln University."
-    const content_2 = "For the past 1.5 years, I have gotten good programming skills and good intern developer experience. The reason why I create this website and looking for a programming job is not only because I just got a Master's degree in Applied Computing from Lincoln University, but also because I like the feeling of creating websites and software. The joy from creating can make me keep moving."
-    const content_3 = "I want to create more websites and software that can bring more joy and convenience to people. If that is also what you want, please see more detail about me. If I am the developer you want, please contact me through my e-mail. I would enjoy having the opportunity to discuss more with you and how I could use my skills to benefit our team. I look forward to hearing from you. "
+    const {currentDate, letterContent} = this.state    
+    // const content_0 = "This is a special cover letter for you. I'm Finn who is from China and a new man for IT. I want to be a full-stack developer, of course, I'm also can do software development if the software is interesting."
+    // const content_1 = "Actually, I was a Hotel Product Manager of the biggest online travel agency (Trip.com Group) in China 2.5 years ago. However, I want to see a more interesting world, so I come to  New Zealand through a WHV chance. I met some developers in my workplace in New Zealand. I found programming in NZ is a very interesting thing after communicating with them, so I start my IT life with the Master of Applied Computing at Lincoln University."
+    // const content_2 = "For the past 1.5 years, I have gotten good programming skills and good intern developer experience. The reason why I create this website and looking for a programming job is not only because I just got a Master's degree in Applied Computing from Lincoln University, but also because I like the feeling of creating websites and software. The joy from creating can make me keep moving."
+    // const content_3 = "I want to create more websites and software that can bring more joy and convenience to people. If that is also what you want, please see more detail about me. If I am the developer you want, please contact me through my e-mail. I would enjoy having the opportunity to discuss more with you and how I could use my skills to benefit our team. I look forward to hearing from you. "
 
 
     return (
@@ -40,10 +48,10 @@ class CoverLetterPage extends Component {
           <div className={covers.letterAsk}>
             <span className={covers.companyName}>Dear {this.props.validateArr[0]} team,</span>
           </div>
-          <div className={covers.letterContent}>{content_0}</div>
-          <div className={covers.letterContent}>{content_1}</div>
-          <div className={covers.letterContent}>{content_2}</div>
-          <div className={covers.letterContent}>{content_3}</div>
+          <div className={covers.letterContent}>{letterContent.content_0}</div>
+          <div className={covers.letterContent}>{letterContent.content_1}</div>
+          <div className={covers.letterContent}>{letterContent.content_2}</div>
+          <div className={covers.letterContent}>{letterContent.content_3}</div>
 
           <div className={covers.letterLast}>Best Regards,<br/> Finn</div>
         </div>
--- frontend/src/pages/ManageEdit_Pg/index.jsx
+++ frontend/src/pages/ManageEdit_Pg/index.jsx
@@ -14,38 +14,39 @@ class ManageEditPage extends Component {
   }
   componentDidMount(){
     // thiseditTableItems is the data that should be in the back-end, and we only neet to take one of them according to the this.props.manageState. don't take all of them like below, below is just temporary solution.
-    const editTableItems = {"CoverLetter":[ "This is a special cover letter for you. I'm Finn who is from China and a new man for IT. I want to be a full-stack developer, of course, I'm also can do software development if the software is interesting.",
-                                           "Actually, I was a Hotel Product Manager of the biggest online travel agency (Trip.com Group) in China 2.5 years ago. However, I want to see a more interesting world, so I come to  New Zealand through a WHV chance. I met some developers in my workplace in New Zealand. I found programming in NZ is a very interesting thing after communicating with them, so I start my IT life with the Master of Applied Computing at Lincoln University.",
-                                           "For the past 1.5 years, I have gotten good programming skills and good intern developer experience. The reason why I create this website and looking for a programming job is not only because I just got a Master's degree in Applied Computing from Lincoln University, but also because I like the feeling of creating websites and software. The joy from creating can make me keep moving.",
-                                           "I want to create more websites and software that can bring more joy and convenience to people. If that is also what you want, please see more detail about me. If I am the developer you want, please contact me through my e-mail. I would enjoy having the opportunity to discuss more with you and how I could use my skills to benefit our team. I look forward to hearing from you. "
+    const editTableItems = {"CoverLetter":[
+                                            ["Paragraph_1","This is a special cover letter for you. I'm Finn who is from China and a new man for IT. I want to be a full-stack developer, of course, I'm also can do software development if the software is interesting."],
+                                            ["Paragraph_2","Actually, I was a Hotel Product Manager of the biggest online travel agency (Trip.com Group) in China 2.5 years ago. However, I want to see a more interesting world, so I come to  New Zealand through a WHV chance. I met some developers in my workplace in New Zealand. I found programming in NZ is a very interesting thing after communicating with them, so I start my IT life with the Master of Applied Computing at Lincoln University."],
+                                            ["Paragraph_3","For the past 1.5 years, I have gotten good programming skills and good intern developer experience. The reason why I create this website and looking for a programming job is not only because I just got a Master's degree in Applied Computing from Lincoln University, but also because I like the feeling of creating websites and software. The joy from creating can make me keep moving."],
+                                            ["Paragraph_4","I want to create more websites and software that can bring more joy and convenience to people. If that is also what you want, please see more detail about me. If I am the developer you want, please contact me through my e-mail. I would enjoy having the opportunity to discuss more with you and how I could use my skills to benefit our team. I look forward to hearing from you. "]
                                           ],
-                            "BasicInfo":{name:"Chiyu He", 
-                                         preferName:"Finn",
-                                         birthday:"Jan 2nd, 1991",
-                                         address:"7/35 Buffon St, Waltham, Christchurch",
-                                         email:"finn.he0102@gmail.com",
-                                         summary:[{id:"s1", content:"Over 50% of courses arrive A grade (4/6). "},
-                                                  {id:"s2", content:"3 web projects development experience - full-stack developer (1 simulation + 2 real )."},
-                                                  {id:"s3", content:"1 desk application development experience - software developer (tkinter )."},
-                                                  {id:"s4", content:"Over half year internship experience."}]
-                                        },
-                            "Education":[
-                                          {
-                                            id:"d0",
-                                            uni:"Xihua University, China ",
-                                            dateMonth:"Sep 2009 - Jun 2013", 
-                                            degree:"Bachelor of Engineering (Automation Engineer Technology/Technician)", 
-                                            magior:"Advanced Mathematics, Neural Networks(basic), PLC, C++(basic), VB(basic ), Analog electronic circuit, Digital electronic circuit, Circuit principle"
-                                          },
-                                          {
-                                            id:"d1",
-                                            uni:"Lincoln University , New Zealand ",
-                                            dateMonth:"Nov 2020 - Feb 2022", 
-                                            degree:"Master of Computer Applications - MCA", 
-                                            magior:"Advanced Programming, Arc GIS, Industry Project, Software Development, Studio Project, Business Analysis"
-                                          }
+                            "BasicInfo":[
+                                         ["Name","Chiyu He"], 
+                                         ["PreferName","Finn"],
+                                         ["Birthday","Jan 2nd, 1991"],
+                                         ["Address","7/35 Buffon St, Waltham, Christchurch"],
+                                         ["Email","finn.he0102@gmail.com"],
+                                         ["Summary_1","Over 50% of courses arrive A grade (4/6). "],
+                                         ["Summary_2","3 web projects development experience - full-stack developer (1 simulation + 2 real )."],
+                                         ["Summary_3","1 desk application development experience - software developer (tkinter )."],
+                                         ["Summary_4","Over half year internship experience."]
                                         ],
-                            "Collections":[["abstract1","type1","address1"], ["abstract2","type2","address2"]]
+                            "Education":[
+                                          ["*","Education_1"],
+                                          ["University","Xihua University, China "],
+                                          ["DateMonth","Sep 2009 - Jun 2013"], 
+                                          ["Degree","Bachelor of Engineering (Automation Engineer Technology/Technician)"], 
+                                          ["Magior","Advanced Mathematics, Neural Networks(basic), PLC, C++(basic), VB(basic ), Analog electronic circuit, Digital electronic circuit, Circuit principle"],
+                                          ["*","Education_2"],
+                                          ["University","Lincoln University , New Zealand "],
+                                          ["DateMonth","Nov 2020 - Feb 2022"], 
+                                          ["Degree","Master of Computer Applications - MCA"], 
+                                          ["Magior","Advanced Programming, Arc GIS, Industry Project, Software Development, Studio Project, Business Analysis"]
+                                        ],
+                            "Collections":[
+                                            ["abstract1","type1","address1"], 
+                                            ["abstract2","type2","address2"]
+                                          ]
                           }
     if (this.props.manageState){
 
