3917a0a69f5e64e6c2ce8cf060454fb8a5e8d3f3
FinnGlobal <finn.he0102@gmail.com>
2022-05-16 22:33:12 +1200

    visit table finish

---
+++ frontend/src/components/ManageVisitorDetail_Com/index.jsx
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from 'react'
+import visitDetailCSS from './index.module.css'
+
+export default class ManageVisitorDetailCOM extends Component {
+  
+ 
+  render() {
+    const {vistdetail} = this.props
+    return (
+      <Fragment>
+          <table className={visitDetailCSS.visitTableDislaly}>
+              <thead  className={visitDetailCSS.visitTHEAD}><tr className={visitDetailCSS.visitTBodyTr}><th>No.</th><th>Company Name</th><th>Visit Date</th><th>Visit Time</th><th style={{width:"15px"}}></th></tr></thead>{/* <th style={{width:"15px"}}></th>  空单元格用于占位和scrollbar一样宽，方便thead和tbody的对应单元格对齐 */}
+              <tbody className={visitDetailCSS.visitTBody}>
+                {vistdetail.map((item, index) => {
+                  return (
+                    <tr key={item.id} className={visitDetailCSS.visitTBodyTr}><td>{index+1}</td><td>{item.companyName}</td><td>{item.visitDate}</td><td>{item.visitTime}</td></tr>
+                  )
+                })}
+              </tbody>
+          </table>
+      </Fragment>
+    )
+  }
+}
--- frontend/src/pages/Management_Pg/index.jsx
+++ frontend/src/pages/Management_Pg/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ManageListCOM from '../../components/ManageList_Com'
+import ManageVisitorDetailCOM from '../../components/ManageVisitorDetail_Com'
 
 export default class ManagementPage extends Component {
   state = {modules:[
@@ -7,14 +8,42 @@ export default class ManagementPage extends Component {
                    {id:"002", content:"Basic Inforamtion Module", moduleName: "BasicInfo"},
                    {id:"003", content:"Education Module", moduleName: "Education"},
                    {id:"004", content:"Collections Module", moduleName: "Collections"},
-                  ]
+                  ],
+           visitors:[
+            {id:"v1", companyName:"aaa", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v2", companyName:"bbb", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v3", companyName:"ccc", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v4", companyName:"ddd", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v5", companyName:"aaa", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v6", companyName:"bbb", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v7", companyName:"ccc", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v8", companyName:"ddd", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v9", companyName:"aaa", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v10", companyName:"bbb", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v11", companyName:"ccc", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v12", companyName:"ddd", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v13", companyName:"aaa", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v14", companyName:"bbb", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v15", companyName:"ccc", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v16", companyName:"ddd", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v17", companyName:"aaa", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v18", companyName:"bbb", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v19", companyName:"ccc", visitDate: "2022-05-16", visitTime:"20:00:00"},
+            {id:"v20", companyName:"ddd", visitDate:  "2022-05-16", visitTime:"20:00:00"},
+           ],
+           visitorsCal:{
+             2022:[[[1, 123],[2, 123],[3, 123],[4, 123],[5, 123],[6, 123],[7, 123],[8, 123],[9, 123],[10, 123],[11, 123],[12, 123],[13, 123],[14, 123],[15, 123],[16, 123],[17, 123],[18, 123],[19, 123],[20, 123],[21, 123],[22, 123],[23, 123],[24, 123],[25, 123],[26, 123],[27, 123],[28, 123],[29, 123],[30, 123],[31, 123]],
+                   [],[],[],[],[],[],[],[],[],[],[]],
+             2023:[],
+             2024:[],
+           }
           }
 
 
 
   render() {
     
-    const {modules} = this.state
+    const {modules, visitors} = this.state
     return (
       <div style={{height:"100%"}}>
         <div style={{ width:"50%", height:"50%", float:"left"}}>
@@ -24,11 +53,15 @@ export default class ManagementPage extends Component {
           })}
         </div>
         <div style={{background:"red", width:"50%", height:"50%", float:"right"}}>
-        visit calendar
+          <ManageVisitorDetailCOM vistdetail= {visitors} />
         </div>
-        <div style={{background:"yellow", width:"100%", height:"50%", float:"left"}}>
-        stay time statistic
+        <div style={{background:"yellow", width:"80%", height:"50%", float:"left"}}>
+        heatmap calendar
         </div>
+        <div style={{background:"green", width:"20%", height:"50%", float:"left"}}>
+        time distribution
+        </div>
+
       </div>
     )
   }
